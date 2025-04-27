a724e83f516aa894ec34b91b55b6352eb9bcc0c9
devisree2004 <devisreepinninti@gmail.com>
2025-04-27 21:45:01 +0530

    practice

---
+++ practice/index.js
@@ -0,0 +1,61 @@
+// console.log("hello");
+// window.alert("i want an internship !");
+// document.getElementById("a").textContent="DEVISREE";
+
+//<-------------------JS BASICS------------------->
+
+//A variable is a container that stores a value
+//1. declaration let x;
+//2. assignment x=100;
+//variable names should be unique
+// let x;
+// x=100;
+// console.log(x);
+//  let x=10;
+//  let y= 20;
+//  console.log(`the value of x ${x} and the value of y is ${y}`);
+//  console.log(typeof x);
+//integer
+//string
+//boolean
+// let online=true;
+// console.log(typeof online);
+
+// document.getElementById("a").textContent=x;
+
+//<-----------------variables------------------------->
+
+//arithmetic operators = operands(values, variables, etc)
+//                       operators(+,-,*,/)
+/*operator precedence
+
+   1.parentheses ()
+   2. exponents
+   3.multiplication, division and modulo 
+   4.addition and subtraction 
+
+*/
+
+//<-----------------operators ------------------------->
+
+/*we can take user input in two ways 
+  1.window.prompt
+  2.professional html way
+*/
+
+// let username;
+// window.prompt("enter the username : ");
+
+let username;
+document.getElementById("submit").onclick = function(){
+    username=document.getElementById("user").value;
+    console.log(username);
+
+}
+
+
+
+//-----------------accept user input ------------------->
+
+
+
